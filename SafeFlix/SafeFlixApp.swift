68a5058b149d17e73a13ab413b46d3c59ecd8e6d
Irham Naufal <inaufal77@gmail.com>
2024-03-13 22:18:22 +0700

    Initial Commit

---
+++ SafeFlix/SafeFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeFlixApp.swift
+//  SafeFlix
+//
+//  Created by Irham Naufal on 13/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SafeFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
